00162f79bebfa3c9656f05279e443bd0088ad6dd
Sebastian Vargas <sebastian.vargas@reign.cl>
2023-07-01 01:45:51 -0500

    feat: add card styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import { WelcomeCard } from "./components/WelcomeCard";
+import { WelcomeCard } from "./components";
+import ellipsisIcon from "./assets/icon-ellipsis.svg";
+import workIcon from "./assets/icon-work.svg";
 
 function App() {
   return (
     <main className="o-container">
       <WelcomeCard />
+      <div className="c-card c-card--Work">
+        <img src={workIcon} className="c-card__icon" />
+        <div className="c-card__body">
+          <div className="c-card__heading">
+            <h5 className="c-card__heading-headline">Work</h5>
+            <button type="button" className="c-card__heading-button">
+              <img src={ellipsisIcon} alt="" />
+            </button>
+          </div>
+          <div className="c-card__data">
+            <span className="c-card__data-current">32hrs</span>
+            <span className="c-card__data-previous">Last Week - 36hrs</span>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/enums/categories.js
@@ -0,0 +1,8 @@
+export const Categories = {
+  WORK: "Work",
+  PLAY: "Play",
+  STUDY: "Study",
+  EXERCISE: "Exercise",
+  SOCIAL: "Social",
+  SELF_CARE: "Self Care",
+};
