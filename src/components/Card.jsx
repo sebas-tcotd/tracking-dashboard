052300dc7918c55df9249387124e862387bf2a20
Sebastian Vargas <sebastian.vargas@reign.cl>
2023-07-01 20:06:00 -0500

    feat: add SEO

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,12 +11,16 @@ export function Card({ category, timeframe }) {
 
   return (
     <div className={`c-card c-card--${category}`}>
-      <img className="c-card__icon" src={iconPath} />
+      <img
+        className="c-card__icon"
+        src={iconPath}
+        alt={`Icon corresponding to the ${category} category`}
+      />
       <div className="c-card__body">
         <div className="c-card__heading">
           <h5 className="c-card__heading-headline">{title}</h5>
           <button type="button" className="c-card__heading-button">
-            <img src={ellipsisIcon} alt="" />
+            <img src={ellipsisIcon} alt="Ellipsis icon" />
           </button>
         </div>
         <div className="c-card__data">
